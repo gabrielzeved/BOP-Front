98e4c3777c7e5fe84271c6cfd2e1ff57d673694a
Frango <canalradiccal@gmail.com>
2021-07-20 20:42:18 -0300

    Initializing...

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <!-- <div id="nav">
+    <router-link to="/">Home</router-link>
   </div>
-  <router-view/>
+  <router-view/> -->
+
+  <div id="main">
+    <svg width="170" height="197" viewBox="0 0 170 197" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4 0H170L154.5 131.514C154.5 131.514 148.246 191.5 143 195C137.754 198.5 99.5 194 99.5 194L45.219 187.519C45.219 187.519 34.7269 184.819 28.3567 178.018C21.9865 171.218 13.3679 151.516 13.3679 151.516L4 131.514V0Z" fill="#FDC82F"/>
+    <g filter="url(#filter0_d)">
+    <path d="M4 0H158L143 122.072C143 122.072 135.367 178.251 130.5 181.5C125.633 184.749 63.7923 176.841 63.7923 176.841L42.2393 174.128C42.2393 174.128 32.5056 171.55 26.5959 165.237C20.6862 158.925 12.6907 140.638 12.6907 140.638L4 122.072V0Z" fill="#008542"/>
+    </g>
+    <defs>
+    <filter id="filter0_d" x="0" y="0" width="162" height="190.283" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+    <feOffset dy="4"/>
+    <feGaussianBlur stdDeviation="2"/>
+    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+    </filter>
+    </defs>
+    </svg>
+  </div>
+
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
 }
+</script>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style scoped lang="scss">
+#app {
 }
 </style>
